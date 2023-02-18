8fb0dbc9f68ba02cac2fd792bd83997a242e23ac
metriquetech <truthtellnolies@gmail.com>
2023-02-17 21:38:17 -0800

    intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main -Entry point
+ * Return: always 0 (success)
+   */
+int main(void)
+{
+	printf("Size of char:lu byte(s),"sizeof(char));
+	printf("Size of an int:lu byte(s),"sizeof(int));
+	printf("Size of long int:lu byte(s),"sizeof(long long int));
+	printf("Size of float:lu byte(s),"sizeof(float));
+	return (0);
+}
