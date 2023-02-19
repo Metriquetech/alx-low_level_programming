a3280763014fbccc798a9e1450d7c851e6bbef0f
metriquetech <truthtellnolies@gmail.com>
2023-02-19 02:09:31 -0800

    print alphabet in lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry
+ *Return -Always zero
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+
+	putchar('\n')
+
+		return (0);
+}
