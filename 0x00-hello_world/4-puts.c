1e322fd02fc6618e6134ed532ce8709e43137a41
metriquetech <truthtellnolies@gmail.com>
2023-02-19 06:44:17 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Sucess)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	return (0);
 }
