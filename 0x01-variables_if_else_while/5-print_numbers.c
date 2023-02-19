237e97db60918addde0896f3cf7ac17651586c7c
metriquetech <truthtellnolies@gmail.com>
2023-02-19 02:28:35 -0800

    print 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry
+ * Return -Always zero
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
