f7276959401f46fea18387912601e3d5fafdd102
metriquetech <truthtellnolies@gmail.com>
2023-02-20 08:25:27 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,17 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = 48; n < 57; n++)
 	{
 		putchar(n);
-		if (n != 57)
+		if (n == 57)
 		{
+			continue;
+		}
 			putchar(';');
 			putchar(' ');
 		}
 		putchar('\n');
 		return (0);
-	}
+
 }
