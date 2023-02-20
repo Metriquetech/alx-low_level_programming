450c7d415ec53a8a6252d60f4afb6cce6679c4eb
metriquetech <truthtellnolies@gmail.com>
2023-02-20 09:25:19 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,9 @@
    */
 int main(void)
 {
-	printf("Size of char:lu byte(s),"sizeof(char));
-	printf("Size of an int:lu byte(s),"sizeof(int));
-	printf("Size of long int:lu byte(s),"sizeof(long long int));
-	printf("Size of float:lu byte(s),"sizeof(float));
+	printf("Size of char:lu byte(s),");sizeof(char);
+	printf("Size of an int:lu byte(s),");sizeof(int);
+	printf("Size of long int:lu byte(s),");sizeof(long long int);
+	printf("Size of float:lu byte(s),");sizeof(float);
 	return (0);
 }
