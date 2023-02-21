5cbc424c06fecbc6d82135288e479c5ce5eea506
metriquetech <truthtellnolies@gmail.com>
2023-02-21 03:32:02 -0800

    print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - Print alphabet
+ */
+void print_alphabet(void);
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+		putchar('\n');
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
+print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
 
 #endif
