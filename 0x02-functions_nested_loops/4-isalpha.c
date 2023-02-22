080b92b0df9e469de7bf9ce2e9f356dc1fd0895a
metriquetech <truthtellnolies@gmail.com>
2023-02-21 16:27:36 -0800

    check 2

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - check for char alphabet
+ * @c : char to be checked
+ * Return : 1 if char is lc or Uc ,otherwise 0
+ */
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || ((c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
