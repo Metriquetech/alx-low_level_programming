6575aa67446af501fd354fed90f6adbf44296d77
metriquetech <truthtellnolies@gmail.com>
2023-02-21 18:46:42 -0800

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs -Computes absolute value
+ * @n: Int in questiion
+ * Return: Absolute valuue of the int
+ */
+
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
+}
