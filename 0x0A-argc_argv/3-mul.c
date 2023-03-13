09a758453962d1c52b01dcdcbabd38e2c8d40f0a
metriquetech <truthtellnolies@gmail.com>
2023-03-13 05:53:53 -0700

    Write a program that adds positive numbers.

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -20,7 +20,7 @@ int _atoi(char *s)
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-			++d;
+		++d;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
@@ -37,4 +37,24 @@ int _atoi(char *s)
 	if (f == 0)
 		return (0);
 	return (n);
+}
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
 }
