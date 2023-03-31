64a0ecb313365ab06c12c7ee0da891dd4ddcd6f7
metriquetech <truthtellnolies@gmail.com>
2023-03-30 17:10:58 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,13 +4,12 @@
 /**
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
- *
  * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
-	
+
 	while (h)
 	{
 		if (!h->str)
